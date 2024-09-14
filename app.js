8c16c52bf576c78e1e37a39f4a776e615a21a498
randombikehelmet <etheaker123@gmail.com>
2024-09-14 18:04:18 +0100

    new

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const images = document.querySelectorAll("img");
 
     for(const image of images){
-        fetch("https://api.thecatapi.com/v1/images/search")
+        fetch("https://api.api-ninjas.com/v1/cats")
         .then(response => response.json())
         .then(data => {
             image.src = data.message
